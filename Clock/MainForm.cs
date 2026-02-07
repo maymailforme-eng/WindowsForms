318d8787e249928304bff25052cd0fcb3c7e822e
Лев <maymailforme@gmail.com>
2026-02-07 21:14:07 +0300

    LoacationDone

--- Clock/MainForm.cs
+++ Clock/MainForm.cs
@@ -37,6 +37,7 @@ namespace Clock
             Directory.SetCurrentDirectory($"{Application.ExecutablePath}\\..\\..\\..");
             string filename = "Settings.ini";
             StreamWriter writer = new StreamWriter(filename);
+            writer.WriteLine($"{this.Location.X}x{this.Location.Y}");
             writer.WriteLine(tsmiTopmost.Checked);
             writer.WriteLine(tsmiShowControls.Checked);
             writer.WriteLine(tsmiShowDate.Checked);
@@ -56,6 +57,9 @@ namespace Clock
             try
             {
                 reader = new StreamReader("Settings.ini");
+                string location = reader.ReadLine();
+                this.Location = new Point(Convert.ToInt32(location.Split('x').First()), Convert.ToInt32(location.Split('x').Last()));
+
                 tsmiTopmost.Checked = bool.Parse(reader.ReadLine());
                 tsmiShowControls.Checked = bool.Parse(reader.ReadLine());
                 tsmiShowDate.Checked = bool.Parse(reader.ReadLine());
